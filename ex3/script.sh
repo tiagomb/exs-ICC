83de3affa46d5e8f054be459587e92d08babb4da
tiagomb <113149976+tiagomb@users.noreply.github.com>
2023-09-24 11:25:17 -0300

    script now filters program output and mflop/s as specified

--- ex3/script.sh
+++ ex3/script.sh
@@ -5,12 +5,11 @@ CPU=3
 
 echo "performance" > /sys/devices/system/cpu/cpufreq/policy3/scaling_governor
 
-make
+make all removeObjects
 
 for k in $METRICA
 do
-    likwid-perfctr -C ${CPU} -g ${k} -m ./interpola $1
+    likwid-perfctr -C ${CPU} -g ${k} -m ./interpola $1 | grep -i "e-\|e+\|mflop/s" | grep -v Runtime | awk {'print $1, $5, $6'}> resultado
 done
-
 
 echo "powersave" > /sys/devices/system/cpu/cpufreq/policy3/scaling_governor 
