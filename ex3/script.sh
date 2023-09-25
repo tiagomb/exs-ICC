d7f9ff29039b0e3dc32d44de71a00755ae82e63c
tiagomb <113149976+tiagomb@users.noreply.github.com>
2023-09-25 18:27:26 -0300

    Add files via upload

--- ex3/script.sh
+++ ex3/script.sh
@@ -5,11 +5,9 @@ CPU=3
 
 echo "performance" > /sys/devices/system/cpu/cpufreq/policy3/scaling_governor
 
-make all removeObjects
-
 for k in $METRICA
 do
-    likwid-perfctr -C ${CPU} -g ${k} -m ./interpola $1 | grep -i "interpola\|e-\|e+\|mflop/s" | grep -v Runtime
+    likwid-perfctr -C ${CPU} -g ${k} -m ./interpola $1 | grep  "interpola\|e-\|e+\|nan\|inf\|MFLOP/s" | grep -v Runtime
 done
 
 echo "powersave" > /sys/devices/system/cpu/cpufreq/policy3/scaling_governor 
